8e6eb1b7fbcb426fe628553e7f06aa501470cd87
Mahmoud Moustafa <mahmoudmoustafa@Mahmouds-MacBook-Air.local>
2026-01-12 23:10:26 -0500

    fix(shared): avoid runtime import of type-only modules

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,14 +1,14 @@
 // Types
-export * from './types';
+export * from './types.js';
 
 // Auth schemas
-export * from './auth.schemas';
+export * from './auth.schemas.js';
 
 // User schemas
-export * from './user.schemas';
+export * from './user.schemas.js';
 
 // Job application schemas
-export * from './job-application.schemas';
+export * from './job-application.schemas.js';
 
 // Activity log schemas
-export * from './activity.schemas';
+export * from './activity.schemas.js';
